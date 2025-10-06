2a020e18fd9154de45a88615741e6d1e8fc10adc
Poojashri7124 <poojashrikumaraguru@gmail.com>
2025-10-06 21:15:14 +0530

    Java program implements the polymorphism using overriding

---
+++ tnsif_java_poojashri/src/oops/polymorphism/overriding/Shape.java
@@ -0,0 +1,12 @@
+package oops.polymorphism.overriding;
+
+public class Shape {
+	public void draw() {
+		System.out.println("Drawing a generic shape");
+	}
+
+	public void erase() {
+		System.out.println("Erasing a generic shape");
+	}
+
+}
