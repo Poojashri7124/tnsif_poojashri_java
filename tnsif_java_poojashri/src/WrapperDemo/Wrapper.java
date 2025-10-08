b82408457993c7de2a14496ea44d765a8d9cbc20
Poojashri7124 <poojashrikumaraguru@gmail.com>
2025-10-08 22:38:25 +0530

    Initial implementation of wrapper

---
+++ tnsif_java_poojashri/src/WrapperDemo/Wrapper.java
@@ -0,0 +1,19 @@
+package WrapperDemo;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		int x = 5;
+		
+		Integer y = Integer.valueOf(x);
+		
+		 
+		
+		int z = y;
+		
+		System.out.println(y +" "+ z);
+
+
+	}
+
+}
