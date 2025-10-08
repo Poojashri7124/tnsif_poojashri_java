de0b70a32808dc79cbce7813f89aed0cbc94114c
Poojashri7124 <poojashrikumaraguru@gmail.com>
2025-10-08 22:35:08 +0530

    Lambda Expression implementation in Java program

---
+++ tnsif_java_poojashri/src/LambdaExpression/Operations.java
@@ -0,0 +1,7 @@
+package LambdaExpression;
+
+public interface Operations {
+	float performArithmetic(int a, int b);
+	
+
+}
---
+++ tnsif_java_poojashri/src/LambdaExpression/Palindrome.java
@@ -0,0 +1,8 @@
+package LambdaExpression;
+
+public interface Palindrome<T> {
+	boolean checkPalindrome(T data);
+
+	
+
+}
